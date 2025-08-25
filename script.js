aa34bac7891dd8953158fc98e2f29ed3b9cb80b3
uchixazokir <uchixaitaachi.71@gmail.com>
2025-08-25 14:34:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+
+let inp1 = document.querySelector('.inp1')
+
+
+let inp2 = document.querySelector('.inp2')
+
+
+let btn = document.querySelector('button')
+
+
+
+btn.addEventListener('click',()=>{
+if(inp1.value == 123 && inp2.value == 2010){
+    alert('Xush kelibsiz')
+}else{
+    alert('Parol hato')
+}
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
